31152e6186a27275e4a7b6913a7061ae97a01aca
Hasnainbhopali <munawarbhopali3@gmail.com>
2026-09-25 13:38:54 +0500

    Add Auth.js session provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "@/components/cart";
 import { Header } from "@/components/Header";
 import { CartDrawer } from "@/components/cart";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CartProvider>
-      <Header />
-      {children}
-      <CartDrawer />
-    </CartProvider>
+    <SessionProvider>
+      <CartProvider>
+        <Header />
+        {children}
+        <CartDrawer />
+      </CartProvider>
+    </SessionProvider>
   );
 }
